852fe160e38b2c23ce2854078bd8d117a6bae825
U-OTN\amcintosh <amcintosh@TORANDREWML.otn.local>
2013-07-17 12:26:28 -0400

    -Looping over exchange sites defined in config file

--- pushoverflow.py
+++ pushoverflow.py
@@ -3,7 +3,7 @@
 import sys
 import configparser
 import requests, json
-from datetime import date, timedelta
+from datetime import datetime, timedelta
 
 __version__ = "0.1"
 __date__ = "2013/16/07"
@@ -15,18 +15,8 @@ __license__ = "GPL"
 STACK_EXCHANGE_BASE_URL = "http://api.stackexchange.com/2.1"
 PUSHOVER_BASE_URL = "https://api.pushover.net/1/messages.json"
 
-def getStackExchangeQuestions(stack_exchange_site, from_date):
-    '''Get new questions posted to stackexchange in past day.
-    '''
-    stack_url = STACK_EXCHANGE_BASE_URL + "/questions"
-    
-    payload = { "fromdate": fromdate, "site": stack_exchange_site}
-    r = requests.get(stack_url, params=payload)
-    print(r.text)	
-    return r.json()
-
-	
 def sendToPushover(config):
+    ''' '''
     payload = { "title":"Test", "message":"This is a test" }
     try:
         payload["token"] = config.get("Pushover","appkey")
@@ -36,14 +26,52 @@ def sendToPushover(config):
         return
 
     requests.post(PUSHOVER_BASE_URL, data=payload)
-    print("Run")	
+    print("Push run: ", payload)	
+
+
+def getStackExchangeQuestions(stack_exchange_site, from_date):
+    '''Get new questions posted to stackexchange in past day.
+    '''
+    stack_url = STACK_EXCHANGE_BASE_URL + "/questions"
+    payload = { "fromdate": int(from_date.timestamp()), "site": stack_exchange_site}
+    print("payload:",payload)
+    r = requests.get(stack_url, params=payload)
+    return r.json()	
+
+def filterQuestions(questions, tags, excluded):
+    for question in questions.get("items"):
+        print(question.get("title"))
 	
-	
+def checkExchanges(config):
+    from_date = None
+    try:
+        time_delta = int(config.get("Global","time_delta_minutes"))
+        from_date = datetime.utcnow() - timedelta(minutes=time_delta)
+    except configparser.NoOptionError as err:
+        print ("Missing properties in configuration file:", err)
+        return
+
+    for section in config.sections():
+        print(section)
+        if section == "Global" or section == "Pushover": 
+            continue
+        exchange = config[section]
+        tags = excluded = []
+        if exchange.get("tags"):
+            tags = exchange.get("tags").split(",")
+        if exchange.get("exclude"):
+            excluded = exchange.get("exclude").split(",")
+        
+        questions = getStackExchangeQuestions(exchange.name, from_date)
+		
+        filterQuestions(questions, tags, excluded)
+		
+		
 def main(argv):
     config = configparser.ConfigParser()
     config.read('pushoverflow.ini')
-    fromdate = date.today() - timedelta(minutes=time_delta)
-    getStackExchangeQuestions("scifi",fromdate)
+    
+    checkExchanges(config)
     #sendToPushover(config)
 
 	
