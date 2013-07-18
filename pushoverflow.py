925bb49ce7fbe649b90ce0e9a67670a632248ef5
U-OTN\amcintosh <amcintosh@TORANDREWML.otn.local>
2013-07-17 23:50:25 -0400

    Working. -Sample config file -Reading from stackexchange and pushing notifications -Supporting priority, devices, urls -TODO: Url title, cleanup code, document, README

--- pushoverflow.py
+++ pushoverflow.py
@@ -15,64 +15,99 @@ __license__ = "GPL"
 STACK_EXCHANGE_BASE_URL = "http://api.stackexchange.com/2.1"
 PUSHOVER_BASE_URL = "https://api.pushover.net/1/messages.json"
 
-def sendToPushover(config):
-    ''' '''
-    payload = { "title":"Test", "message":"This is a test" }
-    try:
-        payload["token"] = config.get("Pushover","appkey")
-        payload["user"] = config.get("Pushover","userkey")
-    except configparser.NoOptionError as err:
-        print ("Missing properties in configuration file:", err)
-        return
-
+def sendToPushover(pushover_config, title, message, url=None):
+    '''Send notification to Pushover'''
+    payload = { "title": title, "message": message }
+    payload["token"] = pushover_config.get("appkey")
+    payload["user"] = pushover_config.get("userkey")
+    payload["priority"] = pushover_config.get("priority")
+    if "device" in pushover_config:
+        payload["device"] = pushover_config.get("device")
+    if url:
+        payload["url"] = url
+    
     requests.post(PUSHOVER_BASE_URL, data=payload)
-    print("Push run: ", payload)	
+    #print("Push run: ", payload)	
 
+def sendQuestionsToPushover(pushover_config, exchange, questions):
+    '''Send notification of new StackExchange questions to Pushover.
+  	   Title will contain the exchange name and message will contain
+       either the number of new questions or the title of the question
+       if only one exists.
+    '''
+    title = "PushOverflow: " + exchange.name
+    if len(questions)==1:
+        message = "New question posted: " + questions[0].get("title")
+        url = questions[0].get("link")
+    else:
+        message = str(len(questions)) + " new questions posted."
+        url = "http://" + exchange.name + ".stackexchange.com/questions?sort=newest"
+    sendToPushover(pushover_config, title, message, url)
 
 def getStackExchangeQuestions(stack_exchange_site, from_date):
-    '''Get new questions posted to stackexchange in past day.
-    '''
+    '''Get new questions posted to stackexchange site since the provided time.'''
     stack_url = STACK_EXCHANGE_BASE_URL + "/questions"
     payload = { "fromdate": int(from_date.timestamp()), "site": stack_exchange_site}
-    print("payload:",payload)
+    #print("payload:",payload)
     r = requests.get(stack_url, params=payload)
     return r.json()	
 
 def filterQuestions(questions, tags, excluded):
+    '''Takes a list of questions and filters them.
+       If tags is not empty, will filter any questions that do not include one of these tags.
+       If excluded is not empty, will filter any questions that include one of these tags.
+    '''
+    filtered_questions = []
     for question in questions.get("items"):
-        print(question.get("title"))
-	
-def checkExchanges(config):
-    from_date = None
-    try:
-        time_delta = int(config.get("Global","time_delta_minutes"))
-        from_date = datetime.utcnow() - timedelta(minutes=time_delta)
-    except configparser.NoOptionError as err:
-        print ("Missing properties in configuration file:", err)
-        return
+        question_tags = question.get("tags")
+        in_tags = False
+        in_excluded = False
+        for tag in question_tags:
+            if len(tags)==0 or tag in tags:
+                in_tags = True
+            if tag in excluded:
+                in_excluded = True
+        if in_tags and not in_excluded:
+            filtered_questions.append(question)
+    return filtered_questions
 
-    for section in config.sections():
-        print(section)
-        if section == "Global" or section == "Pushover": 
-            continue
-        exchange = config[section]
-        tags = excluded = []
-        if exchange.get("tags"):
-            tags = exchange.get("tags").split(",")
-        if exchange.get("exclude"):
-            excluded = exchange.get("exclude").split(",")
-        
-        questions = getStackExchangeQuestions(exchange.name, from_date)
-		
-        filterQuestions(questions, tags, excluded)
-		
+	
+def checkExchange(exchange, from_date):
+    '''Get new questions for this stackexchange site based off of
+       filters provided in configuration file.
+	'''
+    tags = excluded = []
+    if exchange.get("tags"):
+        tags = [x.strip() for x in exchange.get("tags").split(",")]
+    if exchange.get("exclude"):
+        excluded = [x.strip() for x in exchange.get("exclude").split(",")]
+    questions = getStackExchangeQuestions(exchange.name, from_date)
+    questions = filterQuestions(questions, tags, excluded)
+    return questions
 		
 def main(argv):
     config = configparser.ConfigParser()
     config.read('pushoverflow.ini')
-    
-    checkExchanges(config)
-    #sendToPushover(config)
+    from_date = None
+    try:
+        time_delta = int(config.get("Global","time_delta_minutes"))
+        from_date = datetime.utcnow() - timedelta(minutes=time_delta)
+        pushover_app_key = config.get("Pushover","appkey")
+        pushover_user_key = config.get("Pushover","userkey")
+    except configparser.NoOptionError as err:
+        print ("Missing properties in configuration file:", err)
+        return
+
+    except configparser.NoOptionError as err:
+        print ("Missing properties in configuration file:", err)
+        return
+    for section in config.sections():
+        if section == "Global" or section == "Pushover": 
+            continue
+        exchange = config[section]
+        questions = checkExchange(exchange,from_date)
+        if len(questions) > 0:
+            sendQuestionsToPushover(config["Pushover"], exchange, questions)
 
 	
 if __name__ == "__main__":
