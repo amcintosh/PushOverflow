0a9bb9a90c9d507b2e844e2b32f0111970b2215d
Andrew McIntosh <nacho.vonkickbutt@gmail.com>
2013-07-18 22:13:44 -0400

    Some code cleanup

--- pushoverflow.py
+++ pushoverflow.py
@@ -2,7 +2,7 @@
 
 import sys
 import configparser
-import requests, json
+import requests
 from datetime import datetime, timedelta
 
 __version__ = "0.1"
@@ -15,7 +15,7 @@ __license__ = "GPL"
 STACK_EXCHANGE_BASE_URL = "http://api.stackexchange.com/2.1"
 PUSHOVER_BASE_URL = "https://api.pushover.net/1/messages.json"
 
-def sendToPushover(pushover_config, title, message, url=None):
+def send_to_pushover(pushover_config, title, message, url=None):
     '''Send notification to Pushover'''
     payload = { "title": title, "message": message }
     payload["token"] = pushover_config.get("appkey")
@@ -29,7 +29,7 @@ def sendToPushover(pushover_config, title, message, url=None):
     requests.post(PUSHOVER_BASE_URL, data=payload)
     #print("Push run: ", payload)	
 
-def sendQuestionsToPushover(pushover_config, exchange, questions):
+def send_questions_to_pushover(pushover_config, exchange, questions):
     '''Send notification of new StackExchange questions to Pushover.
   	   Title will contain the exchange name and message will contain
        either the number of new questions or the title of the question
@@ -37,22 +37,27 @@ def sendQuestionsToPushover(pushover_config, exchange, questions):
     '''
     title = "PushOverflow: " + exchange.name
     if len(questions)==1:
-        message = "New question posted: " + questions[0].get("title")
+        message = ("New question posted: " 
+                + questions[0].get("title"))
         url = questions[0].get("link")
     else:
         message = str(len(questions)) + " new questions posted."
-        url = "http://" + exchange.name + ".stackexchange.com/questions?sort=newest"
-    sendToPushover(pushover_config, title, message, url)
+        url = ("http://" + exchange.name 
+            + ".stackexchange.com/questions?sort=newest")
+    send_to_pushover(pushover_config, title, message, url)
 
-def getStackExchangeQuestions(stack_exchange_site, from_date):
-    '''Get new questions posted to stackexchange site since the provided time.'''
+def get_stack_exchange_questions(stack_exchange_site, from_date):
+    '''Get new questions posted to stackexchange site since the 
+       provided time.
+    '''
     stack_url = STACK_EXCHANGE_BASE_URL + "/questions"
-    payload = { "fromdate": int(from_date.timestamp()), "site": stack_exchange_site}
+    payload = { "fromdate": int(from_date.timestamp()), 
+                "site": stack_exchange_site}
     #print("payload:",payload)
-    r = requests.get(stack_url, params=payload)
-    return r.json()	
+    res = requests.get(stack_url, params=payload)
+    return res.json()	
 
-def filterQuestions(questions, tags, excluded):
+def filter_questions(questions, tags, excluded):
     '''Takes a list of questions and filters them.
        If tags is not empty, will filter any questions that do not include one of these tags.
        If excluded is not empty, will filter any questions that include one of these tags.
@@ -72,7 +77,7 @@ def filterQuestions(questions, tags, excluded):
     return filtered_questions
 
 	
-def checkExchange(exchange, from_date):
+def check_exchange(exchange, from_date):
     '''Get new questions for this stackexchange site based off of
        filters provided in configuration file.
 	'''
@@ -81,8 +86,8 @@ def checkExchange(exchange, from_date):
         tags = [x.strip() for x in exchange.get("tags").split(",")]
     if exchange.get("exclude"):
         excluded = [x.strip() for x in exchange.get("exclude").split(",")]
-    questions = getStackExchangeQuestions(exchange.name, from_date)
-    questions = filterQuestions(questions, tags, excluded)
+    questions = get_stack_exchange_questions(exchange.name, from_date)
+    questions = filter_questions(questions, tags, excluded)
     return questions
 		
 def main(argv):
@@ -92,8 +97,6 @@ def main(argv):
     try:
         time_delta = int(config.get("Global","time_delta_minutes"))
         from_date = datetime.utcnow() - timedelta(minutes=time_delta)
-        pushover_app_key = config.get("Pushover","appkey")
-        pushover_user_key = config.get("Pushover","userkey")
     except configparser.NoOptionError as err:
         print ("Missing properties in configuration file:", err)
         return
@@ -105,9 +108,9 @@ def main(argv):
         if section == "Global" or section == "Pushover": 
             continue
         exchange = config[section]
-        questions = checkExchange(exchange,from_date)
+        questions = check_exchange(exchange, from_date)
         if len(questions) > 0:
-            sendQuestionsToPushover(config["Pushover"], exchange, questions)
+            send_questions_to_pushover(config["Pushover"], exchange, questions)
 
 	
 if __name__ == "__main__":
