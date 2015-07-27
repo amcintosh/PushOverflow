71e4253236a6b88c6f5c0a52c2e77f9974d75fce
Andrew McIntosh <amcintosh@freshbooks.com>
2015-07-27 07:50:49 -0400

    Fixed tests for both Python2 and 3

--- tests/test_pushover.py
+++ tests/test_pushover.py
@@ -21,18 +21,23 @@ class PushoverTests(unittest.TestCase):
             httpretty.POST, "https://api.pushover.net/1/messages.json",
             status=200
         )
+        test_params = [
+            "title=PushOverflow%3A+TEST", "url=A_LINK_SOMEWHERE",
+            "priority=0", "token=APP_KEY", "user=USER_KEY", "device=DEVICE",
+            "message=New+question+posted%3A+A+TITLE", "url_title=Open+question"
+        ]
+
         questions = [{"title": "A TITLE", "link": "A_LINK_SOMEWHERE"}]
         success = cli.send_questions_to_pushover(
             self.test_config["Pushover"], "TEST", questions)
 
         self.assertTrue(success)
         self.assertEqual(httpretty.last_request().method, "POST")
-        self.assertEqual(
-            httpretty.last_request().body,
-            "title=PushOverflow%3A+TEST&url=A_LINK_SOMEWHERE&priority=0"
-            "&token=APP_KEY&user=USER_KEY&device=DEVICE"
-            "&message=New+question+posted%3A+A+TITLE&url_title=Open+question"
-        )
+
+        body = httpretty.last_request().body
+        self.assertEqual(len(body.rsplit(b"&")), 8)
+        for param in test_params:
+            self.assertTrue(param in str(body))
 
     @httpretty.activate
     def test_send_questions(self):
@@ -40,6 +45,14 @@ class PushoverTests(unittest.TestCase):
             httpretty.POST, "https://api.pushover.net/1/messages.json",
             status=200
         )
+        test_params = [
+            "title=PushOverflow%3A+TEST", "priority=0",
+            "url=http%3A%2F%2FTEST.stackexchange.com%2Fquestions%3Fsort"
+            "%3Dnewest", "token=APP_KEY", "user=USER_KEY",
+            "message=2+new+questions+posted",
+            "url_title=Open+TEST.stackexchange.com"
+        ]
+
         questions = [{"title": "TITLE 1", "link": "LINK_1"},
                      {"title": "TITLE 2", "link": "LINK_2"}]
         del self.test_config["Pushover"]["device"]
@@ -48,14 +61,11 @@ class PushoverTests(unittest.TestCase):
 
         self.assertTrue(success)
         self.assertEqual(httpretty.last_request().method, "POST")
-        self.assertEqual(
-            httpretty.last_request().body,
-            "title=PushOverflow%3A+TEST"
-            "&url=http%3A%2F%2FTEST.stackexchange.com%2Fquestions%3F"
-            "sort%3Dnewest&priority=0&token=APP_KEY&user=USER_KEY"
-            "&message=2+new+questions+posted"
-            "&url_title=Open+TEST.stackexchange.com"
-        )
+
+        body = httpretty.last_request().body
+        self.assertEqual(len(body.rsplit(b"&")), 7)
+        for param in test_params:
+            self.assertTrue(param in str(body))
 
     @httpretty.activate
     def test_failed_send_question(self):
