de3643bc1f4541766bac6063edeb3164cf328d89
Andrew McIntosh <amcintosh@freshbooks.com>
2015-08-25 10:11:13 -0400

    Fixed imports

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,10 +6,12 @@ import sys
 from pushoverflow import cli
 try:
     from unittest.mock import call, patch
-    from io import StringIO
 except ImportError:
     from mock import call, patch
+try:
     from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 
 class CliTests(unittest.TestCase):
